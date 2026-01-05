68c2fb0fce68efb09ca2d15415555ea98020b757
sonsonfun <147006443+sonsonfun@users.noreply.github.com>
2026-01-06 04:57:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def export_high_quality_image(access_token):
         "design_id": DESIGN_ID, 
         "format": {"type": "jpg", "quality": 100}, 
         "pages": [2] 
+        "force_generation": True # もしあれば有効ですが、基本はpages指定で新しいはず
     }
     
     resp = requests.post(url, headers=headers, json=payload)
